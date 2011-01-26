96b74a9d3553469f6ff7c505d32a4d9e0a779730
JP Richardson <jprichardson@gmail.com>
2011-01-26 13:04:09 -0600

    Added saving/loading of lists.

---
+++ CommonLib/Utility/ListUtil.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.ComponentModel;
+
+namespace CommonLib.Utility
+{
+	public static class ListUtil
+	{
+		public static List<V> Load<V>(string file) {
+			return ListUtil.Load<V>(file, TypeDescriptor.GetConverter(typeof(V)));
+		}
+
+		public static List<V> Load<V>(string file, TypeConverter tc) {
+			var sr = new StreamReader(file);
+			var list = new List<V>();
+
+			while (!sr.EndOfStream)
+				list.Add((V)tc.ConvertFromString(sr.ReadLine()));
+
+			sr.Close();
+
+			return list;
+		}
+
+		public static void Save<V>(List<V> self, string file) {
+			var sw = new StreamWriter(file);
+
+			foreach (var val in self)
+				sw.WriteLine(val.ToString());
+
+			sw.Close();
+		}
+	}
+}
---
+++ TestCommonLib/Utility/ListUtilTest.cs
@@ -0,0 +1,51 @@
+﻿using CommonLib.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using CommonLib.Extensions;
+using System.IO;
+
+namespace TestCommonLib
+{
+
+	[TestClass()]
+	public class ListUtilTest
+	{
+		private TestContext testContextInstance;
+
+		public TestContext TestContext {
+			get {
+				return testContextInstance;
+			}
+			set {
+				testContextInstance = value;
+			}
+		}
+
+		[TestMethod()]
+		public void LoadSaveTest() {
+			var list = new List<string>();
+
+			list.Add("1-1");
+			list.Add("2-1");
+			list.Add("3-1");
+			list.Add("1-2");
+			list.Add("2-2");
+			list.Add("2-3");
+
+			var file = Path.GetTempFileName();
+
+			ListUtil.Save(list, file);
+
+			var newList = ListUtil.Load<string>(file);
+
+			Assert.AreEqual(list.Count, newList.Count);
+			
+			Assert.AreEqual(list.Count, newList.Count);
+			for (int x = 0; x < newList.Count; ++x)
+				Assert.AreEqual(list[x], newList[x]);
+			
+		}
+	}
+}
