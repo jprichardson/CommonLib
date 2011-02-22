98f381ab7a338fad4b5a510a52bbd70a454e81f4
JP Richardson <jprichardson@gmail.com>
2011-02-22 10:26:49 -0600

    Added PathUtil and method GetPathAndFileNameWithoutExtension

---
+++ CommonLib/Utility/PathUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CommonLib.Utility
+{
+	public static class PathUtil
+	{
+		public static string GetPathAndFileNameWithoutExtension(string file) {
+			var fn = Path.GetFileNameWithoutExtension(file);
+			var path = Path.GetDirectoryName(file);
+			var ret = "";
+			if (path != "")
+				ret = path + Path.DirectorySeparatorChar + fn;
+			else
+				ret = fn;
+
+			return ret;
+		}
+	}
+}
---
+++ TestCommonLib/Utility/PathUtilTest.cs
@@ -0,0 +1,30 @@
+﻿using CommonLib.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestCommonLib
+{
+
+	[TestClass()]
+	public class PathUtilTest
+	{
+		private TestContext testContextInstance;
+
+		public TestContext TestContext {
+			get {
+				return testContextInstance;
+			}
+			set {
+				testContextInstance = value;
+			}
+		}
+
+		[TestMethod()]
+		public void GetPathAndFileNameWithoutExtensionTest() {
+			var s = @"C:\somedir\somefile.ext";
+			Assert.AreEqual(@"C:\somedir\somefile", PathUtil.GetPathAndFileNameWithoutExtension(s));
+			s = @"somefile.ext";
+			Assert.AreEqual("somefile", PathUtil.GetPathAndFileNameWithoutExtension(s));
+		}
+	}
+}
